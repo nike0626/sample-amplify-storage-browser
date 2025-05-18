e04b2feba20bc84f8fb30bc581786364dcb671fa
nike0626 <42003423+nike0626@users.noreply.github.com>
2025-05-18 17:00:24 +0900

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -17,3 +17,21 @@ export const storage = defineStorage({
     ]
    })
 });
+
+
+export const secondaryStorage = defineStorage({
+  name: 'mySecondaryStorageBucket',
+   access: (allow) => ({
+    'backup_public/*': [
+        allow.guest.to(['read', 'write']),
+        allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+    'backup_admin/*': [
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.authenticated.to(['read'])
+    ],
+    'backup_private/{entity_id}/*': [
+        allow.entity('identity').to(['read', 'write', 'delete'])
+    ]
+   })
+});
